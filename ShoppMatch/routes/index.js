5bad0a9417087164010cea7879306a9d26075c84
Rogerio Martins <rmartins.economia@gmail.com>
2021-09-22 17:26:06 -0300

    add ejs and exclude HTML pages

--- ShoppMatch/routes/index.js
+++ ShoppMatch/routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("home");
+});
+
+router.get("/cadastroproduto", function (req, res, next) {
+  res.render("cadastro-produto");
+});
+
+router.get("/edicaoperfil", function (req, res, next) {
+  res.render("edicao-perfil");
+});
+
+router.get("/cadastrousuario", function (req, res, next) {
+  res.render("cadastro-usuario");
+});
+
+router.get("/contato", function (req, res, next) {
+  res.render("contato");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/pagamento", function (req, res, next) {
+  res.render("pagamento", { title: "Express" });
+});
+
+router.get("/carrinho", function (req, res, next) {
+  res.render("carrinho");
 });
 
 module.exports = router;
