b8caced2b5da3da75e460cdbdbc7b10988d56154
Rogerio Martins <rmartins.economia@gmail.com>
2021-10-03 13:08:48 -0300

    add view and controllers

---
+++ controllers/principalController.js
@@ -0,0 +1,34 @@
+const usuarios = [];
+
+const principalController = {
+	index: function (req, res) {
+		res.render("home");
+	},
+	telaLogin: function (req, res) {
+		res.render("login");
+	},
+	login: function (req, res) {
+		const { email, senha } = req.body;
+		const usuarioLogado = usuarios.find(
+			(usuario) => usuario.email == email && usuario.senha == senha
+		);
+		if (usuarioLogado != null) {
+			res.redirect("/usuario/comprar");
+		} else {
+			res.render("login");
+		}
+	},
+	telaCadastro: function (req, res) {
+		res.render("cadastro-usuario");
+	},
+	cadastrar: function (req, res) {
+		var novoUsuario = req.body;
+		usuarios.push(novoUsuario);
+		res.render("cadastro-sucesso", { novoUsuario });
+	},
+	telaContato: function (req, res) {
+	res.render("contato");
+	},
+};
+
+module.exports = principalController;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var principalController = require("../controllers/principalController.js");
 
-router.get("/", function (req, res, next) {
-	res.render("home");
-});
-
-router.get("/login", function (req, res, next) {
-	res.render("login");
-});
-
-router.get("/cadastrar", function (req, res, next) {
-	res.render("cadastro-usuario");
-});
-
-router.get("/contato", function (req, res, next) {
-	res.render("contato");
-});
+router.get("/", principalController.index);
+router.get("/login", principalController.telaLogin);
+router.post("/login", principalController.login);
+router.get("/cadastrar", principalController.telaCadastro);
+router.post("/cadastrar", principalController.cadastrar);
+router.get("/contato", principalController.telaContato);
 
 module.exports = router;
